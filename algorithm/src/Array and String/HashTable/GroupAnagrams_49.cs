95d295d8a5886976a7022b041b4a063164bd1f5f
vicky <vickyli.dev@gmail.com>
2024-06-08 05:13:44 -0400

    small update for group anagrams

--- algorithm/src/Array and String/HashTable/GroupAnagrams_49.cs
+++ algorithm/src/Array and String/HashTable/GroupAnagrams_49.cs
@@ -1,24 +1,34 @@
 namespace LeetCode.Array_and_String.HashTable;
 
-public class GroupAnagrams_49
+public class GroupAnagrams_49: TestBase
 {
     //Rules:
     //1. Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
     //2. All the characters in one string from string arr is used only once
-    List<IList<string>> GroupAnagrams(string[] strs)
-    {
-        var map = new Dictionary<string, List<string>>();
-        foreach (var str in strs) {
-            var count = new int[26];
-            foreach (var c in str) count[c - 'a']++;
-            var key = string.Join(',', count);// frequency cnt string as key 
-            if (!map.ContainsKey(key)) {
-                map[key] = new List<string>();
+    
+    IList<IList<string>> GroupAnagrams(string[] strs) {
+        var map = new Dictionary<string,List<string>>();
+        foreach(var str in strs){
+            // create key
+            var cnt = new int[26];
+            foreach(var c in str){
+                cnt[c-'a']++;
             }
-            map[key].Add(str);
+            var key = string.Join(',', cnt);
+            TestOutputHelper.WriteLine($"Key for {str} is {key}");
+            if(map.ContainsKey(key)){
+                map[key].Add(str);
+            }else{
+                var list = new List<string>();
+                list.Add(str);
+                map[key] = list;
+            }
+
         }
-        return map.Values.ToList<IList<string>>();
+
+        return map.Values.Select(x=> (IList<string>)x).ToList();
     }
+    
     [Theory]
     [InlineData (new string[] { "eat", "tea", "tan", "ate", "nat", "bat" }, new string[] { "eat", "tea", "ate" }, new string[] { "tan", "nat" }, new string[] { "bat" })]
     public void TestMethod(string[] strs, params string[][] expected)
@@ -33,5 +43,9 @@ public class GroupAnagrams_49
                 Assert.Contains(expected[i][j], result[i]);
             }
         }
+    }
+
+    public GroupAnagrams_49(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
     }
 }
